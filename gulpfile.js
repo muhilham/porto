f664dbf39fa6ca8fd9041ff7b3fd53ae95272f00
Muhammad Ilham <muhilham@go-jek.com>
2016-06-20 15:28:44 +0700

    adding bootstrap and minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 gulp.task('sass', function() {
-    gulp.src('app/scss/*.scss')
+    gulp.src('app/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('app/css'))
-        .pipe(reload({ stream:true }));
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('app/css/main.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('app/styles'))
+    .pipe(reload({ stream:true }));
 });
 
 // watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'minify-css'], function() {
   browserSync({
     server: {
       baseDir: 'app'
@@ -20,4 +27,5 @@ gulp.task('serve', ['sass'], function() {
   });
 
   gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/css/main.css', ['minify-css']);
 });
